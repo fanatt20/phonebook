e8eac78e81a48f181da32cdd288accea661a31fe
Venzhyk Andrey <fanatt20@gmail.com>
2015-08-03 11:34:39 +0300

    init

---
+++ src/PhoneBook/PhoneBook.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhoneBook.WebApp.MvcApplication" Language="C#" %>
---
+++ src/PhoneBook/PhoneBook.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhoneBook.WebApp.Startup))]
+namespace PhoneBook.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
